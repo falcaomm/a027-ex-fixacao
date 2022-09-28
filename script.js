f6fdb3870a9a1d8fa8493295a65b52257e294ebf
falcaomm <manu.m.falcao@gmail.com>
2022-09-28 11:50:57 -0300

    ex fix feedback

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const frutas = ["laranja", "limão", "uva"];
 
-let fruta1var = document.getElementById("fruta-1")
-fruta1var.innerHTML = frutas[0]
+for  (let i = 0; i< frutas.length; i++) {
+    let fruta = document.getElementById(`fruta-${[i+1]}`)
+    fruta.innerHTML = frutas[i]
+}
 
-let fruta2var = document.getElementById("fruta-2")
-fruta2var.innerHTML = frutas[1]
+// let fruta1var = document.getElementById("fruta-1")
+// fruta1var.innerHTML = frutas[0]
 
-let fruta3var = document.getElementById("fruta-3")
-fruta3var.innerHTML = frutas[2]
+// let fruta2var = document.getElementById("fruta-2")
+// fruta2var.innerHTML = frutas[1]
+
+// let fruta3var = document.getElementById("fruta-3")
+// fruta3var.innerHTML = frutas[2]
 
 let fruta4var = document.getElementById("fruta-4")
 
@@ -20,7 +25,6 @@ let imputVar = document.getElementById("imput")
 
 function clicarBotao() {
     fruta4var.innerHTML = imputVar.value
+    console.log(imputVar.value);
     imputVar.value = ""
 }
-
-
